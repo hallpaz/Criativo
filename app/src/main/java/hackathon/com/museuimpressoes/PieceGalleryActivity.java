c41e089dd465b532c0a40a489f334450d7083f9e
Hallison da Paz <hallpaaz@gmail.com>
2015-10-10 20:38:50 -0300

    added gridview

--- app/src/main/java/hackathon/com/museuimpressoes/PieceGalleryActivity.java
+++ app/src/main/java/hackathon/com/museuimpressoes/PieceGalleryActivity.java
@@ -3,6 +3,7 @@ package hackathon.com.museuimpressoes;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.NestedScrollView;
@@ -10,29 +11,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class PieceGalleryActivity extends AppCompatActivity implements View.OnClickListener {
     private final String TAG = "PieceGallery";
 
-    private NestedScrollView imageScrollView;
+    private GridLayout imageGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piece_gallery);
 
-        imageScrollView = (NestedScrollView) findViewById(R.id.imageScrollView);
+        imageGrid = (GridLayout) findViewById(R.id.imageGrid);
 
         BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
-        Bitmap bm = BitmapFactory.decodeFile(getString(R.drawable.sepia_nature), bitmapOptions);
 
-        ImageView image = new ImageView(this);
-        image.setImageBitmap(bm);
+        for(int i = 0; i < 7; ++i){
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.sepia_nature, bitmapOptions);
+            ImageView image = new ImageView(this);
+            image.setImageBitmap(bm);
+            
 
-        imageScrollView.addView(image);
+            imageGrid.addView(image);
 
-        image.setOnClickListener(this);
+            image.setOnClickListener(this);
+        }
 
     }
 
