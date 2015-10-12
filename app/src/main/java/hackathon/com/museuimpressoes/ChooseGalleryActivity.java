a0b200e96b4c44416802c981c8dabfda03078a6d
Kizzy Fernanda Terra Ferreira dos Reis <kizzyterra@gmail.com>
2015-10-12 17:10:11 -0300

    bla

---
+++ app/src/main/java/hackathon/com/museuimpressoes/ChooseGalleryActivity.java
@@ -0,0 +1,13 @@
+package hackathon.com.museuimpressoes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_gallery);
+    }
+}
