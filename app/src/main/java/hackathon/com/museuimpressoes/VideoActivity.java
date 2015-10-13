bc8cd34695315eff9a43dbe8642bdf910b5aa930
Kizzy Fernanda Terra Ferreira dos Reis <kizzyterra@gmail.com>
2015-10-12 21:17:14 -0300

    bla

---
+++ app/src/main/java/hackathon/com/museuimpressoes/VideoActivity.java
@@ -0,0 +1,29 @@
+package hackathon.com.museuimpressoes;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        String uriPath = "android.resource://"+ getPackageName() + "/" +R.raw.description_video;
+        Uri uri = Uri.parse(uriPath);
+
+
+        VideoView myVideoView = (VideoView)findViewById(R.id.myvideoview);
+        myVideoView.setVideoURI(uri);
+        myVideoView.start();
+    }
+
+}
